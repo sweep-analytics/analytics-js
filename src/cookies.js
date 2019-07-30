3f35d7d8102ae6a42b7444edbbe17ba803c64066
Fabian <fabian.bentz@dotsunited.de>
2019-07-30 16:11:49 +0200

    refactor class

--- src/cookies.js
+++ src/cookies.js
@@ -1,26 +1,22 @@
-export default class cookie {
+export function cookieSet(name, value) {
+    const date = new Date();
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    const expires = "; expires=" + date.toUTCString();
 
-    set(name, value) {
-        const date = new Date();
-        date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
-        const expires = "; expires=" + date.toUTCString();
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
 
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-    }
-
-    get(name) {
-        let nameEQ = name + "=";
-        let ca = document.cookie.split(';');
-        for(let i=0;i < ca.length;i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1,c.length);
-            }
-            if (c.indexOf(nameEQ) === 0) {
-                return c.substring(nameEQ.length,c.length);
-            }
+export function cookieGet(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1,c.length);
         }
-        return null;
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
     }
-
+    return null;
 }
--- src/sweep.js
+++ src/sweep.js
@@ -1,75 +1,28 @@
 import uuidv4 from "@bundled-es-modules/uuid/v4.js";
-import cookie from './cookies';
+import { cookieSet, cookieGet } from './cookies';
 
-export default class Sweep {
+export class Sweep {
 
     constructor(apiKey) {
         this.clientId = apiKey;
 
-        this.trackEventMutation = () => `mutation trackEvent($name: String!, $client: String!, $meta: JSON) {
-          trackEvent(input: { name: $name, client: $client, meta: $meta }) { 
-            name,
-            client,
-            meta
-          }
-        }`;
+        window.sweep = {
+            sweepApiKey: apiKey
+        };
+
     }
 
     /*
     * Functions
-    */
+
 
     // Track page views
-    trackPageViews() {
-        if (!cookie.get('s_a_js_uid')) {
-            cookie.set('s_a_js_uid', uuidv4());
-        }
 
-        const url = document.location.pathname;
-        const referrer = document.referrer;
-        const language = navigator.language;
-        const platform = navigator.platform;
-        const screen = `${screen.width}x${screen.height}`;
-
-        const meta = {
-            url,
-            referrer,
-            anonymousId: cookie.get('s_a_js_uid'),
-            language,
-            platform,
-            screen
-        };
-
-        const options = {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                operationName: 'trackEvent',
-                query: this.trackEventMutation(),
-                variables: {
-                    name: 'userSession',
-                    client: this.clientId,
-                    meta
-                }
-            })
-        };
-
-        fetch(`https://api.sweep-analytics.com/graphql`, options)
-            .then((res) => {
-                console.log(res.json());
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-
-    }
 
     // Track events
     trackEvent(event, meta = {}) {
-        if (!cookie.get('s_a_js_uid')) {
-            cookie.set('s_a_js_uid', uuidv4());
+        if (!cookieGet('s_a_js_uid')) {
+            cookieSet('s_a_js_uid', uuidv4());
         }
 
         meta.path = document.location.pathname;
@@ -105,4 +58,68 @@ export default class Sweep {
 
     // Track user flow
     // trackUserDuration() {}
+
+     */
+}
+
+const trackEventMutation = () => `mutation trackEvent($name: String!, $client: String!, $meta: JSON) {
+  trackEvent(input: { name: $name, client: $client, meta: $meta }) { 
+    name,
+    client,
+    meta
+  }
+}`;
+
+export function trackPageViews(screen) {
+    console.log('trackPageViews');
+
+    if (!cookieGet('s_a_js_uid')) {
+        cookieSet('s_a_js_uid', uuidv4());
+    }
+
+    const clientId = window.sweep.sweepApiKey;
+
+    if (!clientId) {
+        throw new Error('No api key provided');
+    }
+
+    const url = document.location.pathname;
+    const referrer = document.referrer;
+    const language = navigator.language;
+    const platform = navigator.platform;
+    const size = `${screen.width}x${screen.height}`;
+
+    const meta = {
+        url,
+        referrer,
+        anonymousId: cookieGet('s_a_js_uid'),
+        language,
+        platform,
+        screen: size
+    };
+
+    const options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            operationName: 'trackEvent',
+            query: trackEventMutation(),
+            variables: {
+                name: 'userSession',
+                client: clientId,
+                meta
+            }
+        })
+    };
+
+    fetch(`https://api.sweep-analytics.com/public`, options)
+            .then((res) => {
+                // console.log(res.json());
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
 }
